b748ebc25e409071ae8faa3d9a9125d85b8fbc63
Pite <33628660@qq.com>
2025-09-19 08:39:26 +0800

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,3 +1,5 @@
+export const config = { runtime: 'edge' };
+
 export default async (req) => {
   const url = 'https://script.google.com/macros/s/AKfycbzij_2rs1gcHjernEZsMBq1CU2HBwl3hPhyGALUFWLMx-4W0xkCNanZQLYi7VauF8td/exec' + req.url.split('/api/proxy')[1];
   const r = await fetch(url, { method: req.method, headers: req.headers });
