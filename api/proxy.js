7738b08163e5a555165121c0e3cfa9cff8c22255
Pite <33628660@qq.com>
2025-09-19 09:24:20 +0800

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,11 +1,19 @@
 export const config = { runtime: 'edge' };
 
 export default async (req) => {
-  const url = 'https://script.google.com/macros/s/AKfycbzij_2rs1gcHjernEZsMBq1CU2HBwl3hPhyGALUFWLMx-4W0xkCNanZQLYi7VauF8td/exec' + req.url.split('/api/proxy')[1];
-  const r = await fetch(url, { method: req.method, headers: req.headers });
-  const body = await r.text();
-  return new Response(body, {
-    status: r.status,
-    headers: { 'Access-Control-Allow-Origin': '*' }
-  });
+  const baseUrl = 'https://script.google.com/macros/s/AKfycbzij_2rs1gcHjernEZsMBq1CU2HBwl3hPhyGALUFWLMx-4W0xkCNanZQLYi7VauF8td/exec';
+  const url = new URL(req.url.substring(req.url.indexOf('/api/proxy') + '/api/proxy'.length), baseUrl).toString();
+  try {
+    const r = await fetch(url, { method: req.method, headers: req.headers });
+    if (!r.ok) {
+      throw new Error(`HTTP error! status = ${r.status}`);
+    }
+    const body = await r.text();
+    return new Response(body, {
+      status: r.status,
+      headers: { 'Access-Control-Allow-Origin': '*' } // 注意修正了这里的格式
+    });
+  } catch (error) {
+    return new Response(error.message || 'Internal Server Error', { status: 500 });
+  }
 };
